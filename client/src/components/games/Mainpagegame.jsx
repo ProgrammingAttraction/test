a9a24e1957f5f1286e4bee635efb6c663a29eeb6
Programming Attraction <abusaishihabmoni@gmail.com>
2026-03-20 02:17:53 +0600

    updated

--- client/src/components/games/Mainpagegame.jsx
+++ client/src/components/games/Mainpagegame.jsx
@@ -22,7 +22,7 @@ import american_roulette_games from "../../assets/american.png";
 import crad_games from "../../assets/card.png";
 import fire_games from "../../assets/fire.png";
 import casino_games from "../../assets/category/Casino.svg";
-import instant_games from "../../assets/category/instant.svg";
+import instant_games from "../../assets/category/Instant.svg";
 import famous_games from "../../assets/category/popular.svg";
 import sports_games from "../../assets/sports.png";
 import live_games from "../../assets/live.png";
